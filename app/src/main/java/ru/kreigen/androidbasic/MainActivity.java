6c5f831128d9aff68624a21f9f577e50aa898256
Kreigen <kreigen8@gmail.com>
2020-08-15 08:47:41 +0300

    1

--- app/src/main/java/ru/kreigen/androidbasic/MainActivity.java
+++ app/src/main/java/ru/kreigen/androidbasic/MainActivity.java
@@ -2,7 +2,27 @@ package ru.kreigen.androidbasic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.ActionMode;
+import android.view.View;
+import android.widget.VideoView;
+//import android.os.Bundle;
+//import android.util.Log;
+//import android.webkit.WebView;
+//import android.webkit.WebViewClient;
+//import android.widget.Toast;
+//import com.google.android.youtube.player.YouTubeBaseActivity;
+//import com.google.android.youtube.player.YouTubeInitializationResult;
+//import com.google.android.youtube.player.YouTubePlayer;
+//import com.google.android.youtube.player.YouTubePlayerView;
+//import com.google.api.client.http.HttpRequest;
+//import com.google.api.client.http.HttpRequestInitializer;
+//import com.google.api.client.http.javanet.NetHttpTransport;
+//import com.google.api.client.json.jackson2.JacksonFactory;
+//import com.google.api.services.youtube.YouTube;
+//import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +30,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void playRickVideo(View view) {
+        VideoView videoView = (VideoView)findViewById(R.id.videoView);
+        String vidAddress = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+        Uri vidUri = Uri.parse(vidAddress);
+        videoView.setVideoURI(vidUri);
+    }
+
+    public void setTxt2(View view) {
+
+    }
+
+    public void setColorBackground(View view) {
+    }
+
+    public void settings(View view) {
+        switch (view.getId()) {
+            case R.id.settings:
+                // TODO Call second activity
+                break;
+            default:
+                break;
+        }
     }
 }
